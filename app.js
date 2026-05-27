159da03fe201607c5d05da9a7cf5df611f6ebb0f
Shivam-Sharma901 <shivamsharmaa226@gmail.com>
2026-05-27 11:44:55 +0530

    hgfgh

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 // CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://smsmern.netlify.app"],
     credentials: true,
   })
 );
